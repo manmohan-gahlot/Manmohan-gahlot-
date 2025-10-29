0c631672c69ced5b9a8ee7154530c3c2ae974ade
Manmohan gahlot <gahlotmanmohan1@gmail.com>
2025-10-29 14:16:12 +0530

    Update sqldata.py

--- sqldata.py
+++ sqldata.py
@@ -1,12 +1,12 @@
 import mysql.connector  
 
-connection = mysql.connector.connect(host='127.0.0.1',user='root',password="Satyampurohit0000" ,database='axixa')
+connection = mysql.connector.connect(host='127.0.0.1',user='root',password="mohan" ,database='axixa')
     
 # print(connection.is_connected())
 
 # cursor = connection.cursor()
 
-# query_to_execute = "SELECT * FROM bacha;"
+# query_to_execute = "SELECT * FROM stu;"
 
 # cursor.execute(query_to_execute)
 # data  = cursor.fetchall()
@@ -17,17 +17,18 @@ connection = mysql.connector.connect(host='127.0.0.1',user='root',password="Saty
 
 cursor = connection.cursor()
 
-name = input("Name dalo: ")
-email = input("Email dalo: ")
-mobile = input("Mobile number dalo: ")
+name = input("enter name: ")
+email = input("Enter email: ")
+mobile = input("Enter Mobile number : ")
 
 
 if email.endswith("@gmail.com"):
-    query = "INSERT INTO bacha (name, email, mobile) VALUES (%s, %s, %s)"
+    query = "INSERT INTO stu (name, email, mobile) VALUES (%s, %s, %s)"
     values = (name, email, mobile)
     cursor.execute(query, values)
     connection.commit()
 
     print("✅ Data inserted successfully!")
 else:
+
     print("❌ Invalid email! Please enter a Gmail address (must end with @gmail.com).")
