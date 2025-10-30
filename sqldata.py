8491d55670ab037c194316ae1eb8b33b2c8046a0
Manmohan gahlot <gahlotmanmohan1@gmail.com>
2025-10-30 16:35:43 +0530

    Update sqldata.py

--- sqldata.py
+++ sqldata.py
@@ -1,25 +1,30 @@
-import mysql.connector  
+import mysql.connector
 
-connection = mysql.connector.connect(host='127.0.0.1',user='root',password="mohan" ,database='axixa')
-    
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    user='root',
+    password="khus",
+    database="python_batch"
+)
+def show_row():
+    cursor = connection.cursor()
+    query = "select name, email, mobile from user order by id asc"
+    cursor.execute(query)
+    while True:
+        user_choice = input("ENTER no. OF ROWS YOU WANT TO SEE:) ")
+        show_rows = int(user_choice)
 
-cursor = connection.cursor()
+        if show_rows == 0:
+            break
+        record = cursor.fetchmany(show_rows)
+        if not record:
+            break
+        for i in record:
+            name, email, mobile = i
+            print(f"Name: {name}")
+            print(f"Email: {email}")
+            print(f"Mobile: {mobile}")
 
-name = input("enter name: ")
-email = input("Enter email: ")
-mobile = input("Enter Mobile number : ")
+    print("All records displayed")
 
-if name.strip() == "" or email.strip() == "" or mobile.strip() == "":
-    print("Name, Email, and Mobile number cannot be empty.")
-    
-elif email.endswith("@gmail.com"):
-    print("Email must end with '@gmail.com'.")
-elif len(mobile) != 10:
-    print("Mobile number must be exactly 10 digits.")
-    
-else:
-    query = "INSERT INTO stu (name, email, mobile) VALUES (%s, %s, %s)"
-    values = (name, email, mobile)
-    cursor.execute(query, values)
-    connection.commit()
-    print("Invalid email! Please enter a Gmail address (must end with @gmail.com)."))
+show_row()
